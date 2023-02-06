a7ca5f75c716a187112bd2e22ab3b24a3722a7d0
杜超 <duchao@hopechart.com>
2023-02-06 20:03:53 +0800

    1

---
+++ MutiPtocessor.py
@@ -0,0 +1,36 @@
+import cv2
+import os
+import argparse
+from tqdm import tqdm
+# import concurrent.futures
+from concurrent.futures import ProcessPoolExecutor
+from multiprocessing import Lock, Pool
+import numpy as np
+
+from utils import *
+
+
+def my_print1(a, b):
+    print(a+b)
+    time.sleep(1)
+
+def my_print2(a):
+    print(a)
+    time.sleep(1)
+    
+a = [1,2,3,4,5,6,7,8]
+b = [1,2,3,4,5,6,7,8]
+
+if __name__ == '__main__':
+    
+
+    MutiProcess_pool = Pool(processes=4)
+    
+    for i, k in zip(a, b):
+        MutiProcess_pool.apply_async(my_print1, (i, k)) # 多个参数，但是不能迭代，只能一个一个加
+    MutiProcess_pool.close()
+    MutiProcess_pool.join()
+    
+    # MutiProcess_pool.map_async(my_print2, a) # 单个参数，可迭代
+
+
