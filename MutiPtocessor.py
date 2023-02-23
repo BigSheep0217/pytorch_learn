28a1a6b972766c34fbc0f244ce7ca87b2a6485e2
杜超 <duchao@hopechart.com>
2023-02-23 16:17:10 +0800

    多进程进度测试

--- MutiPtocessor.py
+++ MutiPtocessor.py
@@ -4,36 +4,45 @@ import argparse
 from tqdm import tqdm
 # import concurrent.futures
 from concurrent.futures import ProcessPoolExecutor
-from multiprocessing import Lock, Pool
+# from multiprocessing import Lock, Pool, Queue
+import multiprocessing as mp
+
 import numpy as np
 
 
 from utils import *
 
 
-def my_print1(a, b):
-    print(a+b)
+def my_print1(l):
+    l.acquire()
+    try:
+        print(os.getpid())
+    finally:
+        l.release()
     time.sleep(1)
+    # return os.getpid()
 
-def my_print2(a):
-    print(a)
-    time.sleep(1)
-    
-a = [1,2,3,4,5,6,7,8]
-b = [1,2,3,4,5,6,7,8]
 
 if __name__ == '__main__':
     
-    cpu_count = os.cpu_count()
+    lock = mp.Manager().Lock() # 进程池的进程不是由当前同一个父进程创建，所以需要借用Manager进行跨进程的管理
+    # task_Q = mp.Manager().Queue()
 
-    MutiProcess_pool = Pool(processes=cpu_count)
-    
-    for i, k in zip(a, b):
-        MutiProcess_pool.apply_async(my_print1, (i, k)) # 多个参数，但是不能迭代，只能一个一个加
+    MutiProcess_pool = mp.Pool(processes=os.cpu_count())    
+    results = []
+    for _ in range(10):
+        results.append(MutiProcess_pool.apply_async(my_print1, args=(lock, ))) # 多个参数，但是不能迭代，只能一个一个加
+        
+    print(f"results : {len(results)}")
+    for result in tqdm(results):
+        result.get() # 阻塞
+        
     MutiProcess_pool.close()
     MutiProcess_pool.join()
     
-    # MutiProcess_pool.map_async(my_print2, a) # 单个参数，可迭代
+    
+
+    
     
 
 
