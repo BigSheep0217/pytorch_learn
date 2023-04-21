f953992d9262ca1ab3ae5224b9e438ef5224e460
杜超 <duchao@hopechart.com>
2023-04-21 13:14:41 +0800

    文件整理

--- utils.py
+++
@@ -1,9 +0,0 @@
-import time
-import datetime
-
-
-def get_million_time_str(prefix='', suffix=''):
-    time.sleep(0.001)
-    date_time = datetime.datetime.now().strftime('%y%m%d%H%M%S')
-    ms = str(int(time.time()*1000)%1000)
-    return prefix + date_time + ms + suffix
